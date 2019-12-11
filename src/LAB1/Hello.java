f3ed77690fd4a153035d9f5c85afbffc2836f4fb
361211760041 <40585901+361211760041@users.noreply.github.com>
2019-12-11 09:05:30 +0700

    Update Hello.java

--- src/LAB1/Hello.java
+++ src/LAB1/Hello.java
@@ -2,7 +2,7 @@ package LAB1;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world I am mouy");
+        System.out.println("Hello world I am kamonchanok");
         System.out.println("MIT");
         System.out.println("361211760041 ");
         System.out.println("kamonchanok ");
