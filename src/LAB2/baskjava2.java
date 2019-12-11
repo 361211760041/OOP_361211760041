b2aac8904377569fa75f30c19d6575e86ddcaf9c
mouy <Mouy0810781674@gmail.com>
2019-12-11 10:50:45 +0700

    test3

---
+++ src/LAB2/baskjava2.java
@@ -0,0 +1,31 @@
+package LAB2;
+
+import java.util.Scanner;
+
+public class baskjava2 {
+    public static void main(String[] args) {
+        //input
+        Scanner sc = new Scanner(System.in);
+
+//        System.out.print("Enter your name: ");
+//        String name =sc.nextLine();
+//        System.out.println("Your name is "+name);
+//
+//        System.out.print("Enter your email:");
+//        String email =sc.nextLine();
+//        System.out.println("Your email is"+email );
+//
+//        System.out.print("Enter Your Age:");
+//        String Age =sc.nextLine();
+//        System.out.println("Your Age is"+Age);
+
+        //input integer and floating
+        System.out.print("Enter an number: ");
+        int num = sc.nextInt();
+        System.out.println("Your entered number:"+num);
+
+        System.out.print("Enter a rual number");
+        double d =sc.nextDouble();
+        System.out.println(d);
+    }
+}
