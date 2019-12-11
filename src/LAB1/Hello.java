28651cce11610d61feb205cf83722934d74794b1
mouy <Mouy0810781674@gmail.com>
2019-12-11 09:04:39 +0700

    test1

--- src/LAB1/Hello.java
+++ src/LAB1/Hello.java
@@ -2,9 +2,9 @@ package LAB1;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world I am ");
+        System.out.println("Hello world I am mouy");
         System.out.println("MIT");
-        System.out.println(" ");
-        System.out.println(" ");
+        System.out.println("361211760041 ");
+        System.out.println("kamonchanok ");
     }
 }
