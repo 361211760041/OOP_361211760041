1606049551b56b1037e84052758150f30d000efd
mouy <Mouy0810781674@gmail.com>
2019-12-11 09:02:35 +0700

    test

---
+++ src/LAB1/Hello.java
@@ -0,0 +1,10 @@
+package LAB1;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world I am ");
+        System.out.println("MIT");
+        System.out.println(" ");
+        System.out.println(" ");
+    }
+}
